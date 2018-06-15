1e32b4ad5b9d9fc8a9dd2fb7ca562365c94c0d50
Krishna Dasari <kmdasari@yahoo.com>
2018-06-15 12:30:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import setuptools
 setuptools.setup(
     name="githubcommit",
     version='0.1.0',
-    url="https://github.com/sat28/githubcommit",
-    author="Shaleen Anand Taneja",
+    url="https://github.com/8451/githubcommit",
+    author="Shaleen Anand Taneja - modified by Krishna Dasari",
     description="Jupyter extension to enable user push notebooks to a git repo",
     packages=setuptools.find_packages(),
     install_requires=[
